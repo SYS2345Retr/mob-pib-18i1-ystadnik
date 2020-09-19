f2b290eefcd8b604f9a2c59f342bf9b97f09f99d
Yaroslav <sdsoerty6584@gmail.com>
2020-09-19 10:47:42 +0600

    Formatting

--- Calc/src/Calc.java
+++ Calc/src/Calc.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 public class Calc {
 
     public static void main(String[] argsOrignal) {
-      try{
-        String charline = "-1.5 + 1e-02";
-        String[] args = charline.split(" ");
-        double x = Double.parseDouble(args[0]);
-        char operation = args[1].charAt(0);
-        double y = Double.parseDouble(args[2]);
-        System.out.println(calc(x, y, operation));}catch (NumberFormatException ex){System.out.println("Invalide format number");}
+        try {
+            String charline = "-1.5 + 1e-02";
+            String[] args = charline.split(" ");
+            double x = Double.parseDouble(args[0]);
+            char operation = args[1].charAt(0);
+            double y = Double.parseDouble(args[2]);
+            System.out.println(calc(x, y, operation));
+        } catch (NumberFormatException ex) {
+            System.out.println("Invalide format number");
+        }
     }
 
     static double calc(double x, double y, char operation) {
