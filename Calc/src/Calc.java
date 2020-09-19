848c2775a210cfdb2f173ba140bebe9aa22994c3
Yaroslav <sdsoerty6584@gmail.com>
2020-09-19 11:42:35 +0600

    Finaly version calc

--- Calc/src/Calc.java
+++ Calc/src/Calc.java
@@ -7,7 +7,6 @@ public class Calc {
             System.out.println("Incorrect arguments length");
             return;
         }
-
         try {
             double x = Double.parseDouble(args[0]);
             char operation = args[1].charAt(0);
