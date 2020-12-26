289232fe347da00a7b3c142efc68eb8da7ae9c60
Yaroslav <sdsoerty6584@gmail.com>
2020-12-26 08:58:02 +0600

    Don't Raynur in project

--- ProjectsAS/app/src/androidTest/java/com/example/stadnikcalc/ExampleInstrumentedTest.java
+++ ProjectsAS/app/src/androidTest/java/com/example/stadnikcalc/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.raynurscalculator", appContext.getPackageName());
+        assertEquals("com.example.stadnikcalculator", appContext.getPackageName());
     }
 }
